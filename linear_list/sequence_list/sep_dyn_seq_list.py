30441ac228bed5ffda2721291ebb53b0cf52c735
Night Cruising <77157236+night-cruise@users.noreply.github.com>
2021-07-08 18:17:05 +0800

    增加插入排序

--- linear_list/sequence_list/sep_dyn_seq_list.py
+++ linear_list/sequence_list/sep_dyn_seq_list.py
@@ -2,6 +2,7 @@
 # -*-coding:UTF-8-*-
 
 """分离式动态顺序表"""
+import random
 
 EXPANSION_MULTIPLE = 1.5  # 当表满时，扩张的倍数
 
@@ -18,7 +19,7 @@ class PList(object):
         if args:
             self._max = int(len(args) * EXPANSION_MULTIPLE)
             self._num = len(args)
-            self._list = [args[i] if i < self._num else None for i in range(self._max)]
+            self._list = [args[i_] if i_ < self._num else None for i_ in range(self._max)]
 
     def is_empty(self):
         """判断表是否为空"""
@@ -138,6 +139,36 @@ class PList(object):
             self._list[i] = self._list[self._num - i - 1]
             self._list[self._num - i - 1] = temp
 
+    def sort(self):
+        """插入排序"""
+        if self._num <= 1:
+            return
+        # j = 1
+        # while j != self.length():
+        #     i_ = 0
+        #     x = self._list[j]
+        #     while i_ != j and self._list[i_] <= self._list[j]:
+        #         i_ += 1
+        #     while i_ != j:
+        #         y = self._list[i]
+        #         self._list[i] = x
+        #         x = y
+        #         i_ += 1
+        #     self._list[j] = x
+        #     j += 1
+
+        # 更好的实现
+        for j in range(1, self.length()):
+            i_ = j - 1
+            while i_ >= 0:
+                if self._list[i_] > self._list[j]:
+                    # 如果list[i] > list[j] 则一直向前交换，目的是把list[j]插入到合适的位置
+                    self._list[i_], self._list[j] = self._list[j], self._list[i_]
+                    i_ -= 1
+                    j -= 1
+                else:
+                    break
+
     def __setitem__(self, key, value):
         if key >= self._num or key < 0:
             raise IndexError("index error")
@@ -153,6 +184,12 @@ class PList(object):
 
     def __len__(self):
         return self._num
+
+    def __contains__(self, item):
+        for i in range(self._num):
+            if self._list[i] == item:
+                return True
+        return False
 
     def __str__(self):
         return str(self._list[0:self._num])
@@ -193,10 +230,16 @@ print(lst)  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 for i in range(11, 17):
     lst.append(i)
-print(lst.length()) # 16
+print(lst.length())  # 16
 print(lst)  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
 for i in range(0, -7, -1):
     lst.prepend(i)
-print(lst.length()) # 23
+print(lst.length())  # 23
 print(lst)  # [-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+lst.clear()
+for i in range(10):
+    lst.append(random.randint(0, i))
+lst.sort()
+print(lst)  # [0, 0, 2, 2, 2, 3, 3, 3, 3, 4]
