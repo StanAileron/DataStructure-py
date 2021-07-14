26cce0abb6290d005051c01cc4d23cb0e7adbcf3
Night Cruising <77157236+night-cruise@users.noreply.github.com>
2021-07-14 21:14:35 +0800

    修正输出coin list错误的bug

--- recursion/coin_change_dynamic.py
+++ recursion/coin_change_dynamic.py
@@ -6,18 +6,20 @@
 from typing import List
 
 
-def coin_change(coin_list: List[int], change: int, coin_counts: List[int], coin_lst: List[int]):
+def coin_change(coin_list: List[int], change: int, coin_counts: List[int], coin_used: List[int]):
     for i in range(1, change + 1):
         min_coin_counts = i  # 初始化当前金额可能需要的最大硬币数
+        coin = 1  # 初始化为了得到当前金额而使用的硬币面值
 
         for j in (j for j in coin_list if j <= i):
-            if coin_counts[i - j] + 1 <= min_coin_counts:  # 通过查表得出i美分减去j硬币后的金额对应的最小硬币数
+            if coin_counts[i - j] + 1 < min_coin_counts:  # 通过查表得出i美分减去j硬币后的金额对应的最小硬币数
                 min_coin_counts = coin_counts[i - j] + 1
-                coin_lst[i] = j
+                coin = j
 
         coin_counts[i] = min_coin_counts
+        coin_used[i] = coin
 
-    return coin_counts[change], coin_lst
+    return coin_counts[change], coin_used
 
 
 def print_coins(coin_list: List[int], change: int):
@@ -37,7 +39,6 @@ if __name__ == '__main__':
 # Need coin counts:  6
 # Need coin:
 # 25 25 10 1 1 1
-# Coin list:  [0, 1, 1, 1, 1, 5, 5, 5, 5, 5, 10, 10, 10, 10, 10, 10, 10,
-# 10, 10, 10, 10, 10, 10, 10, 10, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-# 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
-# 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25]
+# Coin list:  [0, 1, 1, 1, 1, 5, 1, 1, 1, 1, 10, 1, 1, 1, 1, 5, 1, 1, 1,
+# 1, 10, 1, 1, 1, 1, 25, 1, 1, 1, 1, 5, 1, 1, 1, 1, 10, 1, 1, 1, 1, 5, 1,
+# 1, 1, 1, 10, 1, 1, 1, 1, 25, 1, 1, 1, 1, 5, 1, 1, 1, 1, 10, 1, 1, 1]
