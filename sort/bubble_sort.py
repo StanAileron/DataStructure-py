b6f2672390b0bf54e4fd21e639b80bb1ea97edaf
Night Cruising <77157236+night-cruise@users.noreply.github.com>
2021-07-15 22:33:22 +0800

    实现冒泡排序

---
+++ sort/bubble_sort.py
@@ -0,0 +1,25 @@
+#!/user/bin/python
+# -*-coding:UTF-8-*-
+
+
+"""冒泡排序"""
+from typing import List
+
+
+def bubble_sort(alst: List):
+    for i in range(len(alst) - 1):  # 当只有一个元素时不需要比较，因此只需要遍历n-1次。
+        exchange = True
+        for j in range(1, len(alst) - i):
+            if alst[j - 1] > alst[j]:
+                alst[j - 1], alst[j] = alst[j], alst[j - 1]
+                exchange = False
+
+        if not exchange:  # 在这次遍历中没有发生元素交换，说明已经排序好了
+            break
+
+
+if __name__ == '__main__':
+    test_lst = [1, 3, 6, 2, 6, 7, 3, 7, 9, 0, 1, 3, 5, 7, 3, 4]
+    bubble_sort(test_lst)
+    print(test_lst)
+    # [0, 1, 1, 2, 3, 3, 3, 3, 4, 5, 6, 6, 7, 7, 7, 9]
