bac89846f41e31607814da280f2713f854f21d4c
Night Cruising <77157236+night-cruise@users.noreply.github.com>
2021-07-16 20:55:07 +0800

    实现插入排序

---
+++ sort/insert_sort.py
@@ -0,0 +1,23 @@
+#!/user/bin/python
+# -*-coding:UTF-8-*-
+
+
+"""插入排序"""
+from typing import List
+
+
+def insert_sort(alst: List):
+    for i in range(1, len(alst)):
+        j = i - 1
+        while j >= 0:
+            if alst[j] > alst[i]:
+                alst[j], alst[i] = alst[i], alst[j]
+                j, i = j - 1, i - 1
+            else:
+                break
+
+
+if __name__ == '__main__':
+    test_lst = [1, 5, 6, 3, 54, 7, 3, 2, 5, 21, 67, 9, 2, 6, 2]
+    insert_sort(test_lst)
+    print(test_lst)  # [1, 2, 2, 2, 3, 3, 5, 5, 6, 6, 7, 9, 21, 54, 67]
