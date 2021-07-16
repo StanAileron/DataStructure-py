52f616e3c2a99ad09cccb4360370d837919af555
Night Cruising <77157236+night-cruise@users.noreply.github.com>
2021-07-16 23:52:01 +0800

    实现快速排序

---
+++ sort/quick_sort.py
@@ -0,0 +1,55 @@
+#!/user/bin/python
+# -*-coding:UTF-8-*-
+
+
+"""实现快速排序(为基准值寻找正确位置过程的示意图参加（注意，要从左右同时向中间逼近）)
+一. 为基准值为找正确的位置：
+    1. 将列表的第一个元素定义为基准值，设置left和rifht分别表示剩余元素的开头和结尾。
+    2.  （1）比较left和基准值的大小，若基准值>=left，则left右移一位；否则，left止步。
+        （2）然后比较right和基准值的大小，若基准值<=right，则right左移以为；否则，right止步，并且交换left元素和right元素的值。
+        （3）判断left元素和right元素的下标的大小，
+                若left<=right，则重复步骤2；
+                若left>right，则交换right和基准值，并且设置基准值的元素下标为right的下标。以基准值为分割点划分列表，并且递归地调用左列表和右列表。
+
+"""
+from typing import List
+
+
+def partition(alst: List, start: int, end: int) -> int:
+    pivotvalue = alst[start]
+    leftmark = start + 1
+    rightmark = end
+
+    done = False
+    while not done:
+        while leftmark <= rightmark and alst[leftmark] <= pivotvalue:
+            leftmark += 1
+
+        while leftmark <= rightmark and alst[rightmark] >= pivotvalue:
+            rightmark -= 1
+
+        if leftmark <= rightmark:
+            alst[leftmark], alst[rightmark] = alst[rightmark], alst[leftmark]
+        else:
+            done = True
+
+    alst[start], alst[rightmark] = alst[rightmark], alst[start]
+    return rightmark
+
+
+def quick_sort_helper(alst: List, start: int, end: int):
+    if start < end:
+        splitpoint = partition(alst, start, end)
+
+        quick_sort_helper(alst, start, splitpoint - 1)
+        quick_sort_helper(alst, splitpoint + 1, end)
+
+
+def quick_sort(alst: List):
+    quick_sort_helper(alst, 0, len(alst) - 1)
+
+
+if __name__ == '__main__':
+    test_lst = [1, 5, 6, 3, 54, 7, 3, 2, 5, 21, 67, 9, 2, 6, 2]
+    quick_sort(test_lst)
+    print(test_lst)  # [1, 2, 2, 2, 3, 3, 5, 5, 6, 6, 7, 9, 21, 54, 67]
