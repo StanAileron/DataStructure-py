2edd9f2397869875c2008d8e29537c71a6c41150
Night Cruising <77157236+night-cruise@users.noreply.github.com>
2021-07-16 20:48:33 +0800

    实现插入排序

--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -8,11 +8,11 @@ from typing import List
 
 def bubble_sort(alst: List):
     for i in range(len(alst) - 1):  # 当只有一个元素时不需要比较，因此只需要遍历n-1次。
-        exchange = True
+        exchange = False
         for j in range(1, len(alst) - i):
             if alst[j - 1] > alst[j]:
                 alst[j - 1], alst[j] = alst[j], alst[j - 1]
-                exchange = False
+                exchange = True
 
         if not exchange:  # 在这次遍历中没有发生元素交换，说明已经排序好了
             break
