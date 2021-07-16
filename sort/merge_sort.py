8e8e1ef1b8e98b3bdbfbece8649ba7c37e44f488
Night Cruising <77157236+night-cruise@users.noreply.github.com>
2021-07-17 00:55:11 +0800

    实现归并排序(增加注释并修复bug)

--- sort/merge_sort.py
+++ sort/merge_sort.py
@@ -10,7 +10,7 @@ from typing import List
 
 def merge_sort(alst: List):
     print("Splitting ", alst)
-    if len(alst) > 1:
+    if len(alst) > 1:  # 基本结束条件
         mid = len(alst) // 2
         left_lst = alst[:mid]
         right_lst = alst[mid:]
@@ -18,30 +18,30 @@ def merge_sort(alst: List):
         merge_sort(left_lst)
         merge_sort(right_lst)
 
-        i = 0
-        j = 0
-        k = 0
+        i = j = k = 0
+        # 拉链式交错把左半部分从小到大归并到结果列表中
         while i < len(left_lst) and j < len(right_lst):
             if left_lst[i] < right_lst[j]:
                 alst[k] = left_lst[i]
                 i = i + 1
             else:
-                alst[k] = left_lst[j]
+                alst[k] = right_lst[j]
                 j = j + 1
             k = k + 1
 
-        # 要么left_lst有剩余元素，要么right_lst有剩余元素，剩余的元素一定大于alst[k-1]
+        # 归并左半部剩余项
         while i < len(left_lst):
             alst[k] = left_lst[i]
             i = i + 1
             k = k + 1
 
+        # 归并右半部剩余项
         while j < len(right_lst):
             alst[k] = right_lst[j]
             j = j + 1
             k = k + 1
 
-        print("Merging ", alst)
+    print("Merging ", alst)
 
 
 if __name__ == '__main__':
@@ -52,42 +52,57 @@ if __name__ == '__main__':
 # Splitting  [1, 5, 6, 3, 54, 7, 3]
 # Splitting  [1, 5, 6]
 # Splitting  [1]
+# Merging  [1]
 # Splitting  [5, 6]
 # Splitting  [5]
+# Merging  [5]
 # Splitting  [6]
+# Merging  [6]
 # Merging  [5, 6]
 # Merging  [1, 5, 6]
 # Splitting  [3, 54, 7, 3]
 # Splitting  [3, 54]
 # Splitting  [3]
+# Merging  [3]
 # Splitting  [54]
+# Merging  [54]
 # Merging  [3, 54]
 # Splitting  [7, 3]
 # Splitting  [7]
+# Merging  [7]
 # Splitting  [3]
-# Merging  [7, 7]
-# Merging  [3, 3, 54, 54]
-# Merging  [1, 1, 5, 5, 6, 54, 54]
+# Merging  [3]
+# Merging  [3, 7]
+# Merging  [3, 3, 7, 54]
+# Merging  [1, 3, 3, 5, 6, 7, 54]
 # Splitting  [2, 5, 21, 67, 9, 2, 6, 2]
 # Splitting  [2, 5, 21, 67]
 # Splitting  [2, 5]
 # Splitting  [2]
+# Merging  [2]
 # Splitting  [5]
+# Merging  [5]
 # Merging  [2, 5]
 # Splitting  [21, 67]
 # Splitting  [21]
+# Merging  [21]
 # Splitting  [67]
+# Merging  [67]
 # Merging  [21, 67]
 # Merging  [2, 5, 21, 67]
 # Splitting  [9, 2, 6, 2]
 # Splitting  [9, 2]
 # Splitting  [9]
+# Merging  [9]
 # Splitting  [2]
-# Merging  [9, 9]
+# Merging  [2]
+# Merging  [2, 9]
 # Splitting  [6, 2]
 # Splitting  [6]
+# Merging  [6]
 # Splitting  [2]
-# Merging  [6, 6]
-# Merging  [9, 9, 9, 9]
-# Merging  [2, 5, 2, 5, 21, 67, 21, 67]
-# Merging  [1, 1, 1, 1, 5, 5, 5, 5, 6, 6, 54, 54, 67, 21, 67]
+# Merging  [2]
+# Merging  [2, 6]
+# Merging  [2, 2, 6, 9]
+# Merging  [2, 2, 2, 5, 6, 9, 21, 67]
+# Merging  [1, 2, 2, 2, 3, 3, 5, 5, 6, 6, 7, 9, 21, 54, 67]
